d0a0bcdad6e25b9e437cfe7a046707188bc90e31
Remi Duval <contact@remiduval.com>
2021-10-20 19:03:09 +0200

    Topbar with dropdown + buy now/add to cart buttons

--- resources/lang/en/commerce.php
+++ resources/lang/en/commerce.php
@@ -3,10 +3,13 @@
 		'out_of_stock'				=> 'Out of stock',
 		'quantity'                  => 'Quantity',
 		'add_to_cart'               => 'Add to cart',
+		'buy_now'					=> 'Buy now',
 		'price'						=> 'Price',
 		'remove'					=> 'Remove',
 		'continue_to_checkout'		=> 'Continue to checkout',
 		'continue_to_payment'		=> 'Continue to payment',
-        'note_hint'                 => 'Informations supplémentaires à connaître',
+        'note_hint'                 => 'Additional information we should know',
+		'empty_cart'                => 'Empty cart',
+		'start_shopping'            => 'Start shopping',
 	];
 ?>
--- resources/lang/fr/commerce.php
+++ resources/lang/fr/commerce.php
@@ -3,10 +3,13 @@
 		'out_of_stock'				=> 'Épuisé',
         'quantity'                  => 'Quantité',
         'add_to_cart'               => 'Ajout au panier',
+        'buy_now'                   => 'Acheter maintenant',
         'price'                     => 'Prix',
         'remove'					=> 'Supprimer',
 		'continue_to_checkout'		=> 'Étape suivante',
 		'continue_to_payment'		=> 'Continuer vers le paiement',
-        'note_hint'                 => 'Additional information we should know',
+        'note_hint'                 => 'Informations supplémentaires à nous communiquer',
+        'empty_cart'                => 'Panier vide',
+        'start_shopping'            => 'Commencez vos achats',
 	];
 ?>
