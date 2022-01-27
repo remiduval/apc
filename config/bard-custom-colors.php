89f7b74989d45881a21e3561997eece4516e3515
Remi Duval <contact@remiduval.com>
2022-01-27 16:00:55 +0100

    Bard colors settings

---
+++ config/bard-custom-colors.php
@@ -0,0 +1,65 @@
+
+   
+<?php
+
+return [
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Bard Custom Colors
+    |--------------------------------------------------------------------------
+    |
+    | Declare them like this:
+    | 'drunken-penguin' => '#F5F3EF'
+    |
+    | You can also have colors declared like this:
+    | 'red' => [
+    |   '100' => '#fff5f5',
+    |   '200' => '#fed7d7',
+    |   ...
+    |   '900' => '#7b341e',
+    | ]
+    |
+    */
+
+    'custom_colors' => [
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bard Default Colors
+    |--------------------------------------------------------------------------
+    |
+    | This allows you to restrict what colors appear in the default colors section.
+    | If left blank, this will be pulled from the Tailwind defaults.
+    |
+    | Declare them like this:
+    | 'drunken-penguin' => '#F5F3EF'
+    |
+    | You can also have colors declared like this:
+    | 'red' => [
+    |   '100' => '#fff5f5',
+    |   '200' => '#fed7d7',
+    |   ...
+    |   '900' => '#7b341e',
+    | ]
+    |
+    */
+
+
+    'default_colors' => [
+        // 'Orange2' => '#A36800',
+        // 'Green2' => '#2D8429',
+        // 'Purple2' => '#A6599F',
+        // 'Red2' => '#E12F19',
+        // 'Yellow2' => '#947200',
+        'Green'     => '#008000',
+        'Blue'      => '#2574A9',
+        'Pink'      => '#B500B5',
+        'Purple'    => '#58007E',
+        'Yellow'    => '#8D6708',
+        'Orange'    => '#D43900',
+        'Red'       => '#AA0000'
+    ],
+
+];
