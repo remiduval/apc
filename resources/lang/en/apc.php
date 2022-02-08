61566b3b4360f7f58dcabe522ed1f6e75e4f1321
Remi Duval <contact@remiduval.com>
2022-02-08 19:50:28 +0100

    Text if no entries to display

--- resources/lang/en/apc.php
+++ resources/lang/en/apc.php
@@ -1,5 +1,9 @@
 <?php
 	return [
 		'child'             => 'Child',
+		'no_events'			=> 'No events to display',
+		'no_news'			=> 'No news to display',
+		'no_classifieds'	=> 'No classifieds to display',
+		'no_products'		=> 'No products to display',
 	];
 ?>
